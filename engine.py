8bdccd9119d458d5322f0b9825cbb97054e83074
Jonathan Hearn <jonathan-h@chm119.weacceptyou.com>
2018-09-07 12:22:32 +0100

    First commit

---
+++ engine.py
@@ -0,0 +1,106 @@
+"""
+A 3dequalizer4 engine for Tank.
+
+"""
+
+import tank
+import sys
+import traceback
+import re
+import time
+import os
+import logging
+import shutil
+import tempfile
+
+from tank.platform import Engine
+
+class TDE4Engine(Engine):
+    @property
+    def context_change_allowed(self):
+        return True
+
+    @property
+    def host_info(self):
+        host_info = {"name": "3DEqualizer4", "version": "unknown"}
+        try:
+            import tde4
+            host_info['name'], host_info['version'] = re.match("^([^\s]+)\s+(.*)$", tde4.get3DEVersion()).groups()
+        except:
+            # Fallback to initialized above
+            pass
+
+        return host_info
+
+    def create_shotgun_menu(self):
+        if self.has_ui:
+            self.logger.info("Creating Shotgun menu...")
+
+            # create temp dir
+            self.custom_scripts_dir_path = tempfile.mkdtemp()
+
+            for i, (name, command) in enumerate(self.commands.iteritems()):
+                script_path = os.path.join(self.custom_scripts_dir_path, "{:04d}.py".format(i))
+                f = open(script_path, "w")
+                f.write("\n".join((
+                    "# 3DE4.script.name: {}".format(name),
+                    "# 3DE4.script.gui:	Main Window::Shotgun",
+                    "if __name__ == '__main__':",
+                    "   import tank",
+                    "   tank.platform.current_engine().commands[{}]['callback']()".format(repr(name)),
+                )))
+                f.close()
+
+            toks = os.getenv('PYTHON_CUSTOM_SCRIPTS_3DE4', "").split(':')
+            toks.append(self.custom_scripts_dir_path)
+            os.environ['PYTHON_CUSTOM_SCRIPTS_3DE4'] = ":".join(toks)
+
+            import tde4
+            tde4.rescanPythonDirs()
+
+            return True
+        return False
+
+    def post_app_init(self):
+        self.create_shotgun_menu()
+
+    def post_qt_init(self):
+        self._initialize_dark_look_and_feel()
+
+    def post_context_change(self, old_context, new_context):
+        self.create_shotgun_menu()
+
+    def destroy_engine(self):
+        self.logger.debug("%s: Destroying...", self)
+
+        toks = os.getenv('PYTHON_CUSTOM_SCRIPTS_3DE4', "").split(':')
+        toks.remove(self.custom_scripts_dir_path)
+        os.environ['PYTHON_CUSTOM_SCRIPTS_3DE4'] = ":".join(toks)
+
+        shutil.rmtree(self.custom_scripts_dir_path)
+
+    @property
+    def has_ui(self):
+        return True
+
+    ##########################################################################################
+    # logging
+
+    def _emit_log_message(self, handler, record):
+        if record.levelno < logging.INFO:
+            formatter = logging.Formatter("Debug: Shotgun %(basename)s: %(message)s")
+        else:
+            formatter = logging.Formatter("Shotgun %(basename)s: %(message)s")
+
+        msg = formatter.format(record)
+
+        print msg
+
+    def _create_dialog(self, title, bundle, widget, parent):
+        from tank.platform.qt import QtCore
+        dialog = super(TDE4Engine, self)._create_dialog(title, bundle, widget, parent)
+        dialog.setWindowFlags(dialog.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+        dialog.raise_()
+        dialog.activateWindow()
+        return dialog
+
---
+++ startup/startup.py
@@ -0,0 +1,44 @@
+# 3DE4.script.hide: 			true
+# 3DE4.script.startup: 			true
+
+import os
+import sys
+import tde4
+from PySide import QtGui, QtCore
+
+sys.path.append(
+    os.path.join(os.getenv('TANK_CURRENT_PC'), 'install', 'core', 'python')
+)
+import tank
+
+def _timer():
+    QtCore.QCoreApplication.processEvents()
+    # check for open file change
+    global g_current_file
+    cur_file = tde4.getProjectPath()
+    if g_current_file != cur_file:
+        if cur_file:
+            engine = tank.platform.current_engine()
+            context = engine.context
+            new_context = engine.tank.context_from_path(cur_file, context)
+            if new_context != context:
+                tank.platform.change_context(new_context)
+        g_current_file = cur_file
+
+if __name__ == '__main__':
+    engine = tank.platform.current_engine()
+    if not engine:
+        from tank_vendor.shotgun_authentication import ShotgunAuthenticator
+        user = ShotgunAuthenticator(tank.util.CoreDefaultsManager()).get_user()
+        tank.set_authenticated_user(user)
+        context = tank.context.deserialize(os.environ.get("TANK_CONTEXT"))
+        engine = tank.platform.start_engine('tk-3de4', context.tank, context)
+
+    # Qt
+    if not QtCore.QCoreApplication.instance():
+        QtGui.QApplication([])
+        global g_current_file
+        g_current_file = tde4.getProjectPath()
+        tde4.setTimerCallbackFunction("_timer", 10)
+        engine.post_qt_init()
+
