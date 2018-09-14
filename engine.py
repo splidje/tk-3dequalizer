88226b6fd3df4ffb50fb1b69c8cf446e241a3300
Jonathan Hearn <jonathan-h@chm119.weacceptyou.com>
2018-09-14 18:06:18 +0100

    Added helper classes for manipulating 3de4 objects.

--- engine.py
+++ engine.py
@@ -18,32 +18,10 @@ from tank.platform import Engine
 class TDE4Engine(Engine):
     @property
     def context_change_allowed(self):
-        """
-        Whether the engine allows a context change without the need for a restart.
-        """
         return True
 
     @property
     def host_info(self):
-        """
-        :returns: A dictionary with information about the application hosting this engine.
-
-        The returned dictionary is of the following form on success:
-
-            {
-                "name": "Maya",
-                "version": "2017 Update 4",
-            }
-
-        The returned dictionary is of following form on an error preventing
-        the version identification.
-
-            {
-                "name": "Maya",
-                "version: "unknown"
-            }
-        """
-
         host_info = {"name": "3DEqualizer4", "version": "unknown"}
         try:
             import tde4
@@ -84,86 +62,15 @@ class TDE4Engine(Engine):
         return False
 
     def post_app_init(self):
-        """
-        Called when all apps have initialized
-        """
         self.create_shotgun_menu()
-
-        # Run a series of app instance commands at startup.
-        #self._run_app_instance_commands()
 
     def post_qt_init(self):
         self._initialize_dark_look_and_feel()
 
     def post_context_change(self, old_context, new_context):
-        """
-        Runs after a context change. The Maya event watching will be stopped
-        and new callbacks registered containing the new context information.
-
-        :param old_context: The context being changed away from.
-        :param new_context: The new context being changed to.
-        """
         self.create_shotgun_menu()
 
-    def _run_app_instance_commands(self):
-        """
-        Runs the series of app instance commands listed in the 'run_at_startup' setting
-        of the environment configuration yaml file.
-        """
-
-        # Build a dictionary mapping app instance names to dictionaries of commands they registered with the engine.
-        app_instance_commands = {}
-        for (command_name, value) in self.commands.iteritems():
-            app_instance = value["properties"].get("app")
-            if app_instance:
-                # Add entry 'command name: command function' to the command dictionary of this app instance.
-                command_dict = app_instance_commands.setdefault(app_instance.instance_name, {})
-                command_dict[command_name] = value["callback"]
-
-        # Run the series of app instance commands listed in the 'run_at_startup' setting.
-        for app_setting_dict in self.get_setting("run_at_startup", []):
-
-            app_instance_name = app_setting_dict["app_instance"]
-            # Menu name of the command to run or '' to run all commands of the given app instance.
-            setting_command_name = app_setting_dict["name"]
-
-            # Retrieve the command dictionary of the given app instance.
-            command_dict = app_instance_commands.get(app_instance_name)
-
-            if command_dict is None:
-                self.logger.warning(
-                    "%s configuration setting 'run_at_startup' requests app '%s' that is not installed.",
-                    self.name, app_instance_name)
-            else:
-                if not setting_command_name:
-                    # Run all commands of the given app instance.
-                    # Run these commands once Maya will have completed its UI update and be idle
-                    # in order to run them after the ones that restore the persisted Shotgun app panels.
-                    for (command_name, command_function) in command_dict.iteritems():
-                        self.logger.debug("%s startup running app '%s' command '%s'.",
-                                       self.name, app_instance_name, command_name)
-                        maya.utils.executeDeferred(command_function)
-                else:
-                    # Run the command whose name is listed in the 'run_at_startup' setting.
-                    # Run this command once Maya will have completed its UI update and be idle
-                    # in order to run it after the ones that restore the persisted Shotgun app panels.
-                    command_function = command_dict.get(setting_command_name)
-                    if command_function:
-                        self.logger.debug("%s startup running app '%s' command '%s'.",
-                                       self.name, app_instance_name, setting_command_name)
-                        maya.utils.executeDeferred(command_function)
-                    else:
-                        known_commands = ', '.join("'%s'" % name for name in command_dict)
-                        self.logger.warning(
-                            "%s configuration setting 'run_at_startup' requests app '%s' unknown command '%s'. "
-                            "Known commands: %s",
-                            self.name, app_instance_name, setting_command_name, known_commands)
-
-
     def destroy_engine(self):
-        """
-        Stops watching scene events and tears down menu.
-        """
         self.logger.debug("%s: Destroying...", self)
 
         toks = os.getenv('PYTHON_CUSTOM_SCRIPTS_3DE4', "").split(':')
@@ -174,34 +81,12 @@ class TDE4Engine(Engine):
 
     @property
     def has_ui(self):
-        """
-        Detect and return if maya is running in batch mode
-        """
         return True
-        #if cmds.about(batch=True):
-        #    # batch mode or prompt mode
-        #    return False
-        #else:
-        #    return True
 
     ##########################################################################################
     # logging
 
     def _emit_log_message(self, handler, record):
-        """
-        Called by the engine to log messages in Maya script editor.
-        All log messages from the toolkit logging namespace will be passed to this method.
-
-        :param handler: Log handler that this message was dispatched from.
-                        Its default format is "[levelname basename] message".
-        :type handler: :class:`~python.logging.LogHandler`
-        :param record: Standard python logging record.
-        :type record: :class:`~python.logging.LogRecord`
-        """
-        # Give a standard format to the message:
-        #     Shotgun <basename>: <message>
-        # where "basename" is the leaf part of the logging record name,
-        # for example "tk-multi-shotgunpanel" or "qt_importer".
         if record.levelno < logging.INFO:
             formatter = logging.Formatter("Debug: Shotgun %(basename)s: %(message)s")
         else:
@@ -213,19 +98,19 @@ class TDE4Engine(Engine):
         print msg
 
     def _create_dialog(self, title, bundle, widget, parent):
-        """
-        Overriden from the base Engine class - create a TankQDialog with the specified widget 
-        embedded.
-        
-        :param title: The title of the window
-        :param bundle: The app, engine or framework object that is associated with this window
-        :param widget: A QWidget instance to be embedded in the newly created dialog.
-        :param parent: The parent QWidget for the dialog
-        """
         from tank.platform.qt import QtCore
         dialog = super(TDE4Engine, self)._create_dialog(title, bundle, widget, parent)
         dialog.setWindowFlags(dialog.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
         dialog.raise_()
         dialog.activateWindow()
         return dialog
+
+    #############
+    # custom api
+    
+    def iter_all_cameras(self):
+        return self.import_module('tk_3de4').api.TDE4Camera.iter_all()
+
+    def iter_selected_cameras(self):
+        return self.import_module('tk_3de4').api.TDE4Camera.iter_selected()
 
---
+++ python/tk_3de4/api.py
@@ -0,0 +1,99 @@
+import tde4
+
+###
+# CAMERA
+
+class TDE4Camera(object):
+    def __init__(self, cam_id):
+        self._cam_id = cam_id
+
+    def __unicode__(self):
+        return self.name
+
+    @property
+    def name(self):
+        return tde4.getCameraName(self._cam_id)
+    @name.setter
+    def name(self, val):
+        tde4.setCameraName(self._cam_id, val)
+
+    @property
+    def dimensions(self):
+        return (self.width, self.height)
+    @dimensions.setter
+    def dimensions(self, val):
+        self.width = val[0]
+        self.height = val[1]
+
+    @property
+    def width(self):
+        return tde4.getCameraImageWidth(self._cam_id)
+    @width.setter
+    def width(self, val):
+        tde4.setCameraImageWidth(self._cam_id, val)
+
+    @property
+    def height(self):
+        return tde4.getCameraImageHeight(self._cam_id)
+    @height.setter
+    def height(self, val):
+        tde4.setCameraImageHeight(self._cam_id, val)
+
+    @property
+    def image_path(self):
+        return tde4.getCameraPath(self._cam_id)
+    @image_path.setter
+    def image_path(self, val):
+        tde4.setCameraPath(self._cam_id, val)
+
+    @property
+    def frame_range(self):
+        return tde4.getCameraSequenceAttr(self._cam_id)
+    @frame_range.setter
+    def frame_range(self, val):
+        tde4.setCameraSequenceAttr(self._cam_id, val)
+
+    @property
+    def type_(self):
+        return tde4.getCameraType(self._cam_id)
+    @type_.setter
+    def type_(self, val):
+        tde4.setCameraType(self._cam_id, val)
+
+    @property
+    def type_(self):
+        return tde4.getCameraType(self._cam_id)
+    @type_.setter
+    def type_(self, val):
+        tde4.setCameraType(self._cam_id, val)
+
+    @property
+    def frame_count(self):
+        return tde4.getCameraNoFrames(self._cam_id)
+    @frame_count.setter
+    def frame_count(self, val):
+        tde4.setCameraNoFrames(self._cam_id, val)
+
+    @property
+    def lens(self):
+        return TDE4Lens(tde4.getCameraLens(self._cam_id))
+
+    @property
+    def is_selected(self):
+        return tde4.getCameraSelectionFlag(self._cam_id) > 0
+
+    @staticmethod
+    def iter_all():
+        return (TDE4Camera(i) for i in tde4.getCameraList())
+    
+    @staticmethod
+    def iter_selected():
+        return (c for c in TDE4Camera.iter_all() if c.is_selected)
+
+######
+# LENS
+
+class TDE4Lens(object):
+    def __init__(self, lens_id):
+        self._lens_id = lens_id
+
